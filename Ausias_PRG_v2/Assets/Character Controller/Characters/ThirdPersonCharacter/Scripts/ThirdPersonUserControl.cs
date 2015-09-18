5319cc304ba3c83294f3384db5d59f11cf8e2526
jordimc31 <jordiupc87@gmail.com>
2015-09-18 22:33:03 +0200

    character movement II
    walking backwards bug fixed

--- Ausias_PRG_v2/Assets/Character Controller/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Ausias_PRG_v2/Assets/Character Controller/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -48,6 +48,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
+			if (v < 0)
+				v = 0;
+
             bool crouch = Input.GetKey(KeyCode.C);
 
             // calculate move direction to pass to character
@@ -60,6 +63,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             else
             {
                 // we use world-relative directions in the case of no main camera
+
                 m_Move = v*Vector3.forward + h*Vector3.right;
             }
 #if !MOBILE_INPUT
