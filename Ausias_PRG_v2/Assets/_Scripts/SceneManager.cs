6fc9ced45d6f8f1407fcc1db5fd70c27e2f02905
Ruben Sardon <ec4who@hotmail.com>
2015-09-22 14:48:02 +0200

    scene manager start

---
+++ Ausias_PRG_v2/Assets/_Scripts/SceneManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	static SceneManager Instance;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance != null) {
+			GameObject.Destroy (gameObject);
+		}
+		else {
+			GameObject.DontDestroyOnLoad(gameObject);
+			Instance = this;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// scene iput to change
+	
+	}
+}
