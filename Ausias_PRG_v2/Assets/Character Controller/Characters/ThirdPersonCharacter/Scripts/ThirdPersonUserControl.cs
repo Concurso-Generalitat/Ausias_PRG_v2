076cfffa0f050731952479ad8b5572d9cd664cfe
jordimc31 <jordiupc87@gmail.com>
2015-09-27 00:15:21 +0200

    camera/movement

--- Ausias_PRG_v2/Assets/Character Controller/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Ausias_PRG_v2/Assets/Character Controller/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -58,7 +58,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             {
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-				m_Move = v*Vector3.forward + h*Vector3.right;
+				//m_Move = v*Vector3.forward + h*Vector3.right;
+				m_Move = v*m_CamForward + h*m_Cam.right;
             }
             else
             {
